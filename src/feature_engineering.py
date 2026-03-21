bde3eca726a23ae984e420a68a84da14e22516ca
Parth Singh <145176002+pa7003@users.noreply.github.com>
2026-03-21 12:19:19 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -7,12 +7,12 @@ def analyze_text_features(text_series):
     Returns statistics useful for understanding dataset.
     """
 
-    text_lengths = text_series.apply(lambda x: len(x.split()))
+    text_lengths = text_series.apply(lambda x: len(x.split()))    # split sentence into words and then counts number of words
 
     stats = {
-        "Average Length": np.mean(text_lengths),
-        "Max Length": np.max(text_lengths),
-        "Min Length": np.min(text_lengths)
+        "Average Length": np.mean(text_lengths),   # Average number of words per text
+        "Max Length": np.max(text_lengths),        # Longest text in dataset
+        "Min Length": np.min(text_lengths)         # Shortest text in dataset
     }
 
     print("\nText Feature Analysis:")
